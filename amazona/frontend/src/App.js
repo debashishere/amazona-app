520524315ac899d41cf4ee8bffb651b0cb47aad1
debashishere <debashishere007@gmail.com>
2021-07-31 16:53:38 +0530

    adding app component

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,14 +1,16 @@
 import data from './data.js';
+
 function App() {
+
   return (
     <div className="grid-container">
     <header className="row">
       <div>
-        <a className="brand" href="index.html">amazona</a>
+        <a className="brand" href="/">amazona</a>
       </div>
       <div>
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
+        <a href="/cart">Cart</a>
+        <a href="/signin">Sign In</a>
       </div>
     </header>
     <main>
@@ -16,6 +18,7 @@ function App() {
         <div className="row center">
           {
           data.products.map( (product) => (
+
           <div  key={product._id}className="card">
           <a href={` /product/${product._id }`}>
             <img className="medium" src={product.image} alt={product.name} />
@@ -34,6 +37,7 @@ function App() {
             <div className="price">{ `$${product.price}` }</div>
           </div>
         </div>
+
         ))
           }
 
